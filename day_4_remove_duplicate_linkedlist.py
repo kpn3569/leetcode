46516be999a5031f9eecc1b3e539c2f186e2061b
Krishna Prasad Neupane <81921011+kpn3569@users.noreply.github.com>
2022-02-08 23:28:44 -0500

    Create day_4_remove_duplicate_linkedlist.py

---
+++ day_4_remove_duplicate_linkedlist.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=head
+        while prev and prev.next:
+            if prev.next.val == prev.val:
+                prev.next = prev.next.next
+            else:
+                prev = prev.next
+
+        return head
