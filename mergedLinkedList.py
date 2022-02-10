2512a685bd5e18cb85b5e507e2fbab84984c36b6
Krishna Prasad Neupane <81921011+kpn3569@users.noreply.github.com>
2022-02-10 11:33:27 -0500

    Create mergedLinkedList.py

---
+++ mergedLinkedList.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prev=ListNode(0)
+        merge = prev
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                merge.next = list1
+                list1 = list1.next
+            else:
+                merge.next = list2
+                list2 = list2.next
+
+            merge = merge.next
+
+        if list1:
+            merge.next = list1
+        if list2:
+            merge.next = list2
+
+        return prev.next
+        
