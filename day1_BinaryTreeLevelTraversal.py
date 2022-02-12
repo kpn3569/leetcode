7d66e9bec85b28df8ca3ee9c398a4332b3deba19
Krishna Prasad Neupane <81921011+kpn3569@users.noreply.github.com>
2022-02-12 09:29:10 -0500

    Update day1_BinaryTreeLevelTraversal.py

--- day1_BinaryTreeLevelTraversal.py
+++ day1_BinaryTreeLevelTraversal.py
@@ -1,18 +1,23 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        store=dict()
-        ans=[]
-        self.dfs(root,0,store)
-        for _,value in store.items():
-            ans.append(value)
-        return ans
-    
-    def dfs(self,root,level,store):
-        if root:
-            if level in store.keys():
-                store[level]=store[level]+[root.val]
-            else:
-                store[level]=[root.val]
-                
-            self.dfs(root.left,level+1,store)
-            self.dfs(root.right,level+1,store)
+       visited=[]
+        level={}
+        def dfs(root,height):
+            if root:
+                if root not in visited:
+                    visited.append(root)
+                    if height not in level.keys():
+                        level[height]=[root.val]
+                    else:
+                        level[height]=level[height]+[root.val]
+
+                    dfs(root.left,height+1)
+                    dfs(root.right,height+1)
+        
+        dfs(root,0)
+        res=[]
+        for val in level.values():
+            res.append(val)
+        
+        return res
+        
